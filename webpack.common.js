585459f05aebb17e7ca9cedc0c807dbd881f3e83
Matthew Pagan <mastapegs01@gmail.com>
2020-09-22 14:27:59 -0400

    split up webpack config into dev and production

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   entry: './src/index.js',
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'App',
     }),
